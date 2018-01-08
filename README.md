2d4b1b258f8766b6a7bd807ac2f1379640371fc7
Huge Nguyen <nthieu30101985@gmail.com>
2018-01-08 16:05:32 +0700

    Create connection to mongo db server

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # node-mongo
+https://github.com/kevinhooke/SimpleNodeJSExpressApp/blob/master/server.js
+
+https://github.com/simonholmes/mongoose-default-connection
--- app.js
+++ app.js
@@ -3,6 +3,16 @@ var app = express(); //init Express
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/mydbproject');
+
+mongoose.connection.on('error', (err) => {
+    console.error(`MongoDB connection error: ${err}`);
+    process.exit(-1);
+});
+
+mongoose.connection.on('open', (success) => {
+    console.log('Connected to mongo server');
+});
 
 //init bodyParser to extract properties from POST data
 app.use(bodyParser.urlencoded({ extended: true }));
