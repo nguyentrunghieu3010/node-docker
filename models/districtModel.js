c3b68f9bfa1ec2748b8575810ee656a913f48d64
Chuong <chuong.tn@tctav.com>
2018-12-27 10:17:13 +0700

    Revert "first commit"
    This reverts commit ee60ccbcbcc65b652dff5f9aea89fb0c94758552.

--- models/districtModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-let districtSchema = new mongoose.Schema({
-  id: String,
-  districtName: String
-});
-
-module.exports = mongoose.model('District', districtSchema);
